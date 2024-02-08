e76bc1886e6f2029b485a41b14b45820a11c8322
leavelet <leavelet@163.com>
2024-02-08 18:36:57 +0800

    fix: fix for ROCm 6.0

--- compile.sh
+++ compile.sh
@@ -1,8 +1,4 @@
-set -e
-module load rocm
-rm -rf ./build 
-# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DUSE_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES=75 -Bbuild -S. 
-# export CXX=hipcc
+#!/usr/bin/bash
 
-CXX=hipcc cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DUSE_ROCM=ON -Bbuild -S. 
-cd build && make -j
+CXX=hipcc cmake -DCMAKE_PREFIX_PATH="/opt/rocm" -DCMAKE_BUILD_TYPE=Release -DUSE_ROCM=ON -B build -S . 
+cmake --build build -j$(nproc)
